95d0bff05a3794f8737ccdeebec9961f3e6adf0f
dagijosi <dagij@DAGI-PC>
2024-03-23 10:42:51 +0300

    detail

--- src/components/wereda/View.jsx
+++ src/components/wereda/View.jsx
@@ -90,7 +90,7 @@ export const View = () => {
                   className="w-full md:w-1/2 md:mt-4 lg:w-1/3 h-auto px-4"
                 >
                   <Link
-                    to={`/admin/wereda/1`}
+                    to={`/admin/wereda/${item.id}`}
                     className="p-4 pt-9 h-full md:px-7 xl:px-10 bg-white shadow-md  border border-custumBlue hover:shadow-lg hover:bg-mainColor hover:text-white transition duration-300 ease-in-out flex flex-col justify-center relative group  overflow-hidden rounded "
                   >
                     <h4 className="relative z-10 font-semibold font-raleway text-2xl text-dark mb-3">
