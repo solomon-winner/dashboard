d04777c6bcb1cfc6f397ddbec88d8a8a9d532e5c
isume295 <sumeyaibrahim72@.com>
2024-03-31 13:58:01 +0300

    site details integrated

--- src/components/site/Details.jsx
+++ src/components/site/Details.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CloseIcon from '@mui/icons-material/Close';
+import MainLoading from '../Resource/Loading/MainLoading';
+import { useGetSiteByIdQuery } from '../../redux/site/SiteApiSlice';
 
 
 export const SiteDetails = () => {
   const {id} = useParams();
+  const {data, isSuccess,isFetching}=useGetSiteByIdQuery(id)
+  if(!isSuccess || isFetching){
+    return <div className="flex justify-center items-center h-screen">
+    <MainLoading />
+  </div>
+    }
   return (
     <div>
       <div className='flex justify-between p-10'>
@@ -18,15 +26,15 @@ export const SiteDetails = () => {
       <div className="bg-white py-12 sm:py-12">
   <div className="mx-auto max-w-7xl px-6 lg:px-8">
     <div className="mx-auto max-w-2xl sm:text-center">
-      <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Site Name: Agar Jefjefa</h2>
+      <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Site Name: {data.data.site_name}</h2>
       {/* <p className="mt-6 text-lg leading-8 text-gray-600">Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.</p> */}
     </div>
     <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
       <div className="p-8 text-gray-600 sm:p-10 lg:flex-auto">
-        <h3 className="text-2xl font-bold tracking-tight text-customDark ">Kebele: Agar</h3>
-        <h3 className="text-2xl font-bold tracking-tight text-customDark ">Micro-washed: Agar Wenz</h3>
-        <h3 className="text-2xl font-bold tracking-tight text-customDark ">Degraded land restoration: jefjefa</h3>
-        <h3 className="text-2xl font-bold tracking-tight text-customDark ">Size of site: 9.61ha</h3>
+        <h3 className="text-2xl font-bold tracking-tight text-customDark ">Kebele ID: {data.data.kebele_id}</h3>
+        <h3 className="text-2xl font-bold tracking-tight text-customDark ">Micro-washed: {data.data.watershed_name}</h3>
+        {/* <h3 className="text-2xl font-bold tracking-tight text-customDark ">Degraded land restoration: jefjefa</h3> */}
+        <h3 className="text-2xl font-bold tracking-tight text-customDark ">Size of site: {data.data.size_ha} ha</h3>
 
 <div className="mt-10 flex items-center gap-x-4">
 
@@ -34,50 +42,14 @@ export const SiteDetails = () => {
 <div className="h-px flex-auto bg-gray-100"></div>
 </div>
 <ul role="list" className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
-<li className="flex gap-x-3">
-   <CloseIcon className='text-red-500'/>
-  Farm land
-</li>
-<li className="flex gap-x-3">
-  <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+{data.data.resources[0]?.LAND?.map((item, index) => (
+           <li className="flex gap-x-3">
+ <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
     <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
   </svg>
-  Communal grazing
+  {item.value}
 </li>
-<li className="flex gap-x-3">
-<CloseIcon className='text-red-500'/>
-  Forest land
-</li>
-<li className="flex gap-x-3">
-<CloseIcon className='text-red-500'/>
-
-  Bush land
-</li>
-
-<li className="flex gap-x-3">
-<CloseIcon className='text-red-500'/>
-
-  Shrub land
-</li>
-
-<li className="flex gap-x-3">
-  <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-  Degraded land
-</li>
-
-<li className="flex gap-x-3">
-<CloseIcon className='text-red-500'/>
-
-  Settlement  
-</li>
-
-<li className="flex gap-x-3">
-<CloseIcon className='text-red-500'/>
-
-  Other land use
-</li>
+         ))}
 </ul>
 
 <div className="mt-10 flex items-center gap-x-4">
@@ -86,54 +58,16 @@ export const SiteDetails = () => {
 <div className="h-px flex-auto bg-gray-100"></div>
 </div>
 <ul role="list" className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
-<li className="flex gap-x-3">
-<svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-  Cardio Africana
-</li>
-<li className="flex gap-x-3">
-  <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-  Olia Africana
-</li>
-<li className="flex gap-x-3">
-<svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-  Rhamnus priniodes
-</li>
-<li className="flex gap-x-3">
-<svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
 
-  Croton macrstachyus
-</li>
-
-<li className="flex gap-x-3">
-<svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-
-  Capparis tomentosa
-</li>
-
-<li className="flex gap-x-3">
-  <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-  Milletia ferruginea
-</li>
-
-<li className="flex gap-x-3">
-<svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-
-  Juniperus procera 
-</li>
+       {data.data.resources[1]?.TREE?.filter(tree => tree.indigenous === 1).map((tree, index) => (
+           <li key={index} className="flex gap-x-3">
+        <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+        </svg>
+       {tree.value}
+      </li>
+        ))   
+        }
 </ul>
 
 
@@ -148,98 +82,51 @@ export const SiteDetails = () => {
 <div className="h-px flex-auto bg-gray-100"></div>
 </div>
 <ul role="list" className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
-<li className="flex gap-x-3">
-<svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-  Acacia decurrens
-</li>
-<li className="flex gap-x-3">
-  <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-  Grevillea robusta
-</li>
-<li className="flex gap-x-3">
-<svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-  Cupressus equisetifolia
-</li>
-<li className="flex gap-x-3">
-<svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-  </svg>
-
-  Sesbania sesban
-</li>
+{ data.data.resources[1]?.TREE?.filter(tree => !tree.hasOwnProperty('indigenous')).map((tree, index) => (
+           <li  key={index} className="flex gap-x-3">
+           <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+             <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+           </svg>
+           {tree.value}
+         </li>
+))}
 </ul>
 
         <div className="mt-10 flex items-center gap-x-4">
 
-        <h4 className="flex-none text-sm font-semibold leading-6 text-customDark">Livelihood activities</h4>
+        <h4 className="flex-none text-sm font-semibold leading-6 text-customDark">Livestock </h4>
         <div className="h-px flex-auto bg-gray-100"></div>
         </div>
 
         <ul role="list" className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
-          <li className="flex gap-x-3">
-            <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Poultry
-          </li>
-          <li className="flex gap-x-3">
-            <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Bee keeping
-          </li>
-          <li className="flex gap-x-3">
-            <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Small ruminants
-          </li>
-          <li className="flex gap-x-3">
-            <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-              <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-            </svg>
-            Forage/fodder growing
-          </li>
+
+       { data.data.resources[2]?.LIVESTOCK?.map((item, index) => (
+           <li className="flex gap-x-3">
+ <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+  </svg>
+  {item.value}
+</li>
+         ))}
         </ul>
 
 
         <div className="mt-10 flex items-center gap-x-4">
 
-<h4 className="flex-none text-sm font-semibold leading-6 text-customDark">Livelihood activities</h4>
+<h4 className="flex-none text-sm font-semibold leading-6 text-customDark">Forage</h4>
 <div className="h-px flex-auto bg-gray-100"></div>
 </div>
 
 <ul role="list" className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
-  <li className="flex gap-x-3">
-    <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-    </svg>
-    Grass
-  </li>
-  <li className="flex gap-x-3">
-    <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-    </svg>
-    Trulucern
-  </li>
-  <li className="flex gap-x-3">
-    <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-    </svg>
-    Suspania
-  </li>
-  <li className="flex gap-x-3">
-    <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-      <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
-    </svg>
-    Elephant grass
-  </li>
+
+{data.data.resources[3]?.FORAGE?.map((item, index) => (
+           <li className="flex gap-x-3">
+ <svg className="h-6 w-5 flex-none text-customDark" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+    <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+  </svg>
+  {item.value}
+</li>
+         ))}
 </ul>
    
  
