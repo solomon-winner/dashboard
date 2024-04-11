19334e74f15973a6e7238d40bd776f983170db7e
solomon-winner <solyalew3@gmail.com>
2024-04-11 16:35:18 +0300

    ...

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,12 +8,22 @@ import { useGetResource } from '../redux/InitialState/GetResource'
 import { GetRoles } from '../redux/InitialState/GetRoles';
 import {ProfileInfo} from "../redux/InitialState/ProfileInfo"
 import { LocationInfo } from '../components/Maps/LocationInfo'
+import { useDispatch, useSelector } from 'react-redux'
+import { SetZoom_out } from '../redux/GeoJson/GeoJsonSlice'
 export const Dashboard = () => {
+  const dispatch = useDispatch()
+  const Zoom = useSelector((state) => state.geoJson.GeoJson.Zoom_out);
   useGetRegion();
   GetRoles();
   useGetInstitution();
   useGetResource();
   ProfileInfo()
+  const Zoom_out = (e) =>        {
+    e.preventDefault();
+    dispatch(SetZoom_out(true));
+    console.log("The zoom inside the Zoom_out...", Zoom)
+
+  }
   return (
     <div className='flex flex-col h-screen gap-6 bg-dashbordColor'> 
       <Cards />
@@ -21,6 +31,8 @@ export const Dashboard = () => {
         <div className='w-8/12 h-18'>
           <h1 className='text-xl font-semibold'>Degraded sites Map</h1>
           <div className='p-3'>
+            <button className='bg-green-500 text-white font-bold py-2 px-4  hover:bg-darkMain  rounded-md' onClick={Zoom_out}>Zoom Out</button>
+           {  console.log("this is zoom", Zoom)}
             <Map />
           </div>
           </div>
