38eaaa3daddd3dde2ee6b5325a8fdde913082873
dagijosi <dagij@DAGI-PC>
2024-05-15 15:01:15 +0300

    back

--- src/components/Resource/Utility/BackButton.jsx
+++ src/components/Resource/Utility/BackButton.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const BackButton = () => {
+  // const goBack = () => {
+  //   window.history.back();
+  // };
   const goBack = () => {
+    // Get the previous URL in the history
+    const previousUrl = document.referrer;
+
+    // Define the prefixes to skip
+    const prefixesToSkip = [
+      "/admin/update-siteData",
+      "/admin/update-site",
+      "/admin/update-kebeleData",
+      "/admin/update-kebele",
+      "/admin/update-weredaData",
+      "/admin/update-wereda",
+    ];
+
+    // Check if the previous URL starts with any of the prefixes to skip
+    for (const prefix of prefixesToSkip) {
+      if (previousUrl.includes(prefix)) {
+        // If the previous URL starts with a prefix, go back twice to skip it
+        window.history.go(-3);
+        return; // Exit the function to prevent further processing
+      }
+    }
+
+    // If the previous URL doesn't start with any of the prefixes, go back once
     window.history.back();
   };
 
@@ -16,4 +42,3 @@ const BackButton = () => {
 };
 
 export default BackButton;
-
