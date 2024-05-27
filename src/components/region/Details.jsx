109e802a3a58846f135e4cab8b5284da80b1ee03
dagijosi <dagij@DAGI-PC>
2024-05-27 14:35:39 +0300

    responsive

--- src/components/region/Details.jsx
+++ src/components/region/Details.jsx
@@ -77,7 +77,7 @@ export const RegionDetails = () => {
                 name={"woreda_name"}
                 title={"Woreda"}
                 urlName={"wereda"}
-                className={"grid grid-cols-4 gap-1"}
+                className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1"}
               />
             </div>
           </div>
