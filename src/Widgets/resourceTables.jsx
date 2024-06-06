ce72413498d14fce3efec6e05bae3e71d6a44a60
solomon-winner <solyalew3@gmail.com>
2024-06-06 14:07:39 +0300

    render table rows

---
+++ src/Widgets/resourceTables.jsx
@@ -0,0 +1,19 @@
+const ResourceTable = ({ resources, resourceName }) => {
+    return (
+      <>
+        <h4>{resourceName}</h4>
+        <hr />
+        <table className="table-auto w-full">
+          <tbody>
+            {resources.map((resource, index) => (
+              <tr key={index}>
+                <td className="border px-4 py-2 font-bold">{resource.value}</td>
+                <td className="border px-4 py-2">{resource.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <hr />
+      </>
+    );
+  };
