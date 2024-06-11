27f4c05509e73334e72a551e7019c260191bf023
solomon-winner <solyalew3@gmail.com>
2024-06-11 12:06:59 +0300

    ...

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,23 +6,10 @@ import { useGetInstitution } from '../redux/InitialState/GetInstitution'
 import { useGetResource } from '../redux/InitialState/GetResource'
 import { GetRoles } from '../redux/InitialState/GetRoles';
 import {ProfileInfo} from "../redux/InitialState/ProfileInfo"
-import { useSelector } from "react-redux";
-import { KebeleLocationInfo } from '../components/LocationInfo/KebeleInfo'
-import { SiteLocationInfo } from '../components/LocationInfo/SiteInfo'
-import { RegionLocationInfo } from '../components/LocationInfo/RegionInfo'
-import { WoredaLocationInfo } from '../components/LocationInfo/WoredaInfo'
-import { Default, LocationInfo } from '../components/Maps/LocationInfo'
+import { LocationInfo } from '../components/Maps/LocationInfo'
 
 export const Dashboard = () => {
 
- 
-  const Region_id  = useSelector((state) => state.geoJson.GeoJson.SelectedRegion);
-  const defualtLocation  = useSelector((state) => state.geoJson.GeoJson.LocationInfo);
-  const Site_id  = useSelector((state) => state.geoJson.GeoJson.SelectedSite);
-  const Woreda_id = useSelector((state) => state.geoJson.GeoJson.SelectedWoreda);
-  const Kebele_id = useSelector((state) => state.geoJson.GeoJson.SelectedKebele);
-
-console.log("Woreda_id",Woreda_id)
   useGetRegion();
   GetRoles();
   useGetInstitution();
