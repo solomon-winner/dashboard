8357cd2e0f0de262dcd5031cd5d1cf5759384d6b
solomon-winner <solyalew3@gmail.com>
2024-06-11 11:46:22 +0300

    ...

--- src/Widgets/LiveliHoodTable.jsx
+++ src/Widgets/LiveliHoodTable.jsx
@@ -10,17 +10,17 @@ const LivelihoodTable = ({ livelihoods }) => {
       <table className="table-auto w-full">
         <thead>
           <tr>
-            <th className="px-4 py-2 text-lg">Type</th>
-            <th className="px-4 py-2 text-lg">Male Headed HH</th>
-            <th className="px-4 py-2 text-lg">Female Headed HH</th>
+            <th className="px-2 py-2 text-lg">Type</th>
+            <th className="px-2 py-2 text-lg">Male Headed HH</th>
+            <th className="px-2 py-2 text-lg">Female Headed HH</th>
           </tr>
         </thead>
         <tbody>
           {livelihoods.map((livelihood, index) => (
             <tr key={index}>
-              <td className="border px-4 py-2 font-bold">{livelihood.value}</td>
-              <td className="border px-4 py-2">{livelihood.male_headed_hh}</td>
-              <td className="border px-4 py-2">{livelihood.female_headed_hh}</td>
+              <td className="border px-2 py-2 font-bold">{livelihood.value}</td>
+              <td className="border px-2 py-2">{livelihood.male_headed_hh}</td>
+              <td className="border px-2 py-2">{livelihood.female_headed_hh}</td>
             </tr>
           ))}
         </tbody>
