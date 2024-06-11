2f3c7f6eb9609b9fb4af0afcff8f582a239439a8
solomon-winner <solyalew3@gmail.com>
2024-06-11 10:25:15 +0300

    ...

--- src/components/Maps/FetchGeoJsonMap.js
+++ src/components/Maps/FetchGeoJsonMap.js
@@ -22,5 +22,17 @@ export const fetchRegionData = async (url) => {
     }
   };
 
-  
-//https://tbrr.echnoserve.com/api/geojson/regions/13/woredas
+  export const Get_Coordinates = async (geoJSON) =>{
+    try {
+      const response = await fetch(`https://tbrr.echnoserve.com${geoJSON}`);
+      const data = await response.json();
+      if (data.features && data.features.length > 0 && data.features[0].geometry && data.features[0].geometry.coordinates.length > 0) {
+        return data.features[0].geometry.coordinates[0][0];
+      } else {
+        return "No Coordinate";
+      }
+    } catch (error) {
+      console.error('Error fetching coordinates:', error);
+      return "No Coordinate";
+    }
+  }
