a6298c808c45aa2e39656bd768ea1517ae989aed
solomon-winner <solyalew3@gmail.com>
2024-06-11 11:44:13 +0300

    ...

--- src/components/Maps/LocationInfo.jsx
+++ src/components/Maps/LocationInfo.jsx
@@ -5,25 +5,6 @@ import { useSelector } from "react-redux";
 import { KebeleLocationInfo } from "../LocationInfo/KebeleInfo";
 import { RegionLocationInfo } from "../LocationInfo/RegionInfo";
 
-export const Default = () => {
-  return(
-    <div className="d-flex min-w-80">
-    <div className="w-50" style={{ border: '1px solid gray' }}>
-      <div className="container project-container">
-        <div className="card">
-        <div className="bg-gray-200 border-gray-400">
-            <p className="text-lg font-bold ml-5 py-3">Detailed location Information</p>
-          </div>
-          <div className="card-body px-4">
-            <p>Select a region to view detailed location information.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
 export const LocationInfo = () => {
   const Region_id  = useSelector((state) => state.geoJson.GeoJson.SelectedRegion);
   const defualtLocation  = useSelector((state) => state.geoJson.GeoJson.LocationInfo);
@@ -40,15 +21,18 @@ export const LocationInfo = () => {
             <p className="text-lg font-bold ml-5 py-3">Detailed location Information</p>
           </div>
           <div className="card-body" style={{ overflowX: 'auto', overflowY: 'auto', maxHeight: '80vh' }}>
-            {defualtLocation && <Default/>}
+            {defualtLocation &&          
+             <div className="card-body px-4">
+            <p>Select a region to view detailed location information.</p>
+            </div>}
             {Region_id && <RegionLocationInfo/>}
             {Site_id && <SiteLocationInfo/>}
             {Woreda_id && <WoredaLocationInfo/>}
             {Kebele_id && <KebeleLocationInfo/>}
             </div>
-            </div>
-            </div>
-            </div>
-            </div>
+           </div>
+          </div>
+        </div>
+      </div>
   )
 }
