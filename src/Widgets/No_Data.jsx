610c10463e662d5e1179ce95be8a6aad7c7dfa38
solomon-winner <solyalew3@gmail.com>
2024-06-21 12:25:29 +0300

    ...

--- src/Widgets/No_Data.jsx
+++ src/Widgets/No_Data.jsx
@@ -1,7 +1,10 @@
-const No_Data = (resourceName) => {
+import EmptyComponent from "../components/Resource/Utility/EmptyComponent"
+
+export const No_Data = (resourceName) => {
     return(
         <>
-        
-        </>
+         <h4 className="px-9 py-2 font-bold text-sm">{resourceName}:</h4>
+         <EmptyComponent/>
+         </>
     )
 }
