7be1701b989bb5f1c378c7aa0d16eeeaddeaa3a8
solomon-winner <solyalew3@gmail.com>
2024-06-21 13:33:22 +0300

    ...

--- src/Widgets/No_Data.jsx
+++ src/Widgets/No_Data.jsx
@@ -1,10 +1,10 @@
-import EmptyComponent from "../components/Resource/Utility/EmptyComponent"
-
 export const No_Data = ({resourceName}) => {
     return(
         <>
          <h4 className="px-9 py-2 font-bold text-sm">{resourceName}:</h4>
-         <EmptyComponent/>
+         <div className="flex justify-center items-center bg-gray-100 rounded-md p-4">
+            <p className="text-gray-500 font-semibold">No {resourceName} information available.</p>
+         </div>
          </>
     )
 }
